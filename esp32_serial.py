ed00dffb4322666efc78f980627383d534788d71
Arshad Husain <arshad.husain1@gmail.com>
2022-12-10 12:14:22 -0700

    Add current datetime to serial output

--- esp32_serial.py
+++ esp32_serial.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 import serial
+import datetime
 ser = serial.Serial('/dev/ttyS0',115200)
 while True:
-    readedText = ser.readline()
-    print(readedText.decode("utf-8"), end='')
+    try:
+        readedText = ser.readline()
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        byte_line = readedText.decode("utf-8")
+        print(f"{current_time}:  {byte_line}", end='')
+    except Exception:
+        print("Unable to read serial")
 ser.close()
