73ff40bc500be037d49238ab0d9fe8eeeb5d312e
Arshad Husain <arshad.husain1@gmail.com>
2022-12-10 12:24:36 -0700

    Clean up code

--- esp32_serial.py
+++ esp32_serial.py
@@ -6,10 +6,13 @@ ser = serial.Serial('/dev/ttyS0',115200)
 logfile = open("esp32-logs.txt", 'a')
 while True:
     try:
-        readedText = ser.readline()
         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        readedText = ser.readline()
         byte_line = readedText.decode("utf-8")
+
         line_to_save = f"{current_time}:  {byte_line}"
+
         print(line_to_save, end='')
         logfile.write(line_to_save)
     except Exception:
