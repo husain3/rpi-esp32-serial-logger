857dd0dcf12b534d94dfb942528fd137b290ae44
Arshad Husain <arshad.husain1@gmail.com>
2022-12-10 12:21:47 -0700

    Add logfile to write to

--- esp32_serial.py
+++ esp32_serial.py
@@ -2,12 +2,17 @@
 import serial
 import datetime
 ser = serial.Serial('/dev/ttyS0',115200)
+
+logfile = open("esp32-logs.txt", 'a')
 while True:
     try:
         readedText = ser.readline()
         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         byte_line = readedText.decode("utf-8")
-        print(f"{current_time}:  {byte_line}", end='')
+        line_to_save = f"{current_time}:  {byte_line}"
+        print(line_to_save, end='')
+        logfile.write(line_to_save)
     except Exception:
         print("Unable to read serial")
+
 ser.close()
